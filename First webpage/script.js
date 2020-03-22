85dcc085ffb98865532252962d2d853002ff01fb
Dizza-code <61956163+Dizza-code@users.noreply.github.com>
2020-03-22 15:27:01 +0000

    First webpage

--- First webpage/script.js
+++ First webpage/script.js
@@ -1,9 +1,13 @@
-<script> 
+<script type="text/javascript">   
 var myName = "Amos";
 var myCourses = ["frontend", "Design", "Coding"];
 console.log(myName);
 console.log(myCourses);
 
-
+var oneToTwoHundred = [];
+for (var h = 1; h < 200; h += 2) {
+  oneToTwoHundred.push(h)
+}
+console.log(oneToTwoHundred);
 
 </script>
